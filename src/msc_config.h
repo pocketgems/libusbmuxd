9dfc43a52d76f5d828d713505af06f8b15822240
Frederik Carlier <frederik.carlier@quamotion.mobi>
2017-11-04 11:35:15 +0100

    Visual C++: Define _CRT_NONSTDC_NO_DEPRECATE

--- src/msc_config.h
+++ src/msc_config.h
@@ -23,6 +23,7 @@
 #ifdef _MSC_VER
 #define inline __inline
 #define WIN32_LEAN_AND_MEAN
+#define _CRT_NONSTDC_NO_DEPRECATE
 #define __func__ __FUNCTION__
 #define strncasecmp _strnicmp
 #define strcasecmp _stricmp
